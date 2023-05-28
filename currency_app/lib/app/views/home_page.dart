465ea01478dbbd0f3a05d46d966e0b33d9a41c0e
italo <italogithub@gmail.com>
2023-05-28 03:48:09 -0300

    projeto criado

---
+++ currency_app/lib/app/views/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 20),
+          child: Column(
+            children: [
+              Image.asset('assets/logo.png', width: 150, height: 150),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: DropdownButton(items: const [
+                          DropdownMenuItem(child: Text('Real')),
+                          
+                          DropdownMenuItem(child: Text('Dólar'))
+                        ], onChanged: (value) {})),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Expanded(
+                      flex: 2,
+                      child: TextField(
+                        decoration: InputDecoration(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(onPressed: () => {}, child: Text('Converter'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
